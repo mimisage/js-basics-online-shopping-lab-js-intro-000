30354016ce608880a9057f3c3ad02719e02d53a4
Michelle Sage <michelle2sage@gmail.com>
2019-10-30 14:53:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,36 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart.push({[item]: Math.floor(Math.random()*100)})
- console.log(`${item} has been added to your cart.`)
+  cart.push({[item]:[price]})
  return cart
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-     return console.log("Your shopping cart is empty.")
-   }
-   var string = "In your cart, you have ";
-
-  if (cart.length === 1) {
-    string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
-  } else if (cart.length === 2) {
-    string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
-  } else {
-    for (var i=0; i<cart.length; i++) {
-      var text;
-      if (i === cart.length - 2){
-          text = ", and ";
-      } else if (i === cart.length - 1) {
-          text = "";
-      } else {
-          text = ", ";
-      }
-      string += `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}` + text + `${i === cart.length - 1 ? '.' : ''}`;
-    }
-  }
-
-  return console.log(string);
 }
 
 
