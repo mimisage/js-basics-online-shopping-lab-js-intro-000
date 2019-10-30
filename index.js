e4c31a7fbbf07b507da64fcb08c979adfd594067
Michelle Sage <michelle2sage@gmail.com>
2019-10-30 14:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,34 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length == 0) {
+    console.log('Your shopping cart is empty.')
+  } else {
+    var message = 'In your cart, you have '
+    var item = cart[0]
+    var itemName = Object.keys(item)[0]
+    var itemPrice = item[itemName]
+    message += `${itemName} at $${itemPrice}`
+    if(cart.length == 2) {
+      var item2 = cart[1]
+      var itemName2 = Object.keys(item2)[0]
+      var itemPrice2 = item2[itemName2]
+      message += ` and ${itemName2} at $${itemPrice2}`
+    } else if(cart.length >= 3) {
+      for(let i = 1; i < cart.length - 1; i++) {
+        var varItem = cart[i]
+        var varItemName = Object.keys(varItem)[0]
+        var varItemPrice = varItem[varItemName]
+        message += `, ${varItemName} at $${varItemPrice}`
+      }
+      var lastItem = cart[cart.length - 1]
+      var lastItemName = Object.keys(lastItem)[0]
+      var lastItemPrice = lastItem[lastItemName]
+      message += `, and ${lastItemName} at $${lastItemPrice}`
+    }
+    message += '.'
+    console.log(message)
+  }
 }
 
 function total() {
